3cf6ed9c0a8ef72188f4b7c8edd1c110c38874d5
marchellotpp12 <marchellotpp12@gmail.com>
2026-09-10 15:48:08 +0700

    71251227

---
+++ UG1-AnalisisAlgoritma-Rekursif/Soal#2/SistemKopdes.py
@@ -0,0 +1,67 @@
+# import ini jangan dihapus/diedit yak
+import random
+
+
+def totalPenjualan(data, n):
+    if n == 0:
+        return 0
+    else:
+        namabarang, jumlah = data[n - 1]
+        return jumlah + totalPenjualan(data, n - 1)
+
+
+def penjualanTertinggi(data, n):
+    if n == 1:
+        return data[0]
+    else:
+        sisaTertinggi = penjualanTertinggi(data, n - 1)
+        namaBarang, jumlah = data[n - 1]
+        if jumlah > sisaTertinggi[1]:
+            return (namaBarang, jumlah)
+        else:
+            return sisaTertinggi
+
+
+def diAtasRataRata(penjualan, rataRata):
+    jumlahDiAtas = 0
+    for namaBarang, jumlah in penjualan.items():
+        if jumlah > rataRata:
+            jumlahDiAtas += 1
+    return jumlahDiAtas
+
+
+# Program Utama - Jangan dihapus/diedit yak
+angka = int(input("NIM: "))
+random.seed(angka)
+
+barang = [
+    "Beras",
+    "Minyak",
+    "Gula",
+    "Telur",
+    "Kopi",
+    "Teh"
+]
+
+penjualan = {}
+
+for namaBarang in barang:
+    penjualan[namaBarang] = random.randint(100, 500)
+
+data = list(penjualan.items())
+n = len(data)
+
+print("\n===== Data Penjualan =====")
+for namaBarang, jumlah in penjualan.items():
+    print(namaBarang, ":", jumlah)
+
+total = totalPenjualan(data, n)
+tertinggi = penjualanTertinggi(data, n)
+rataRata = total / n
+jumlahDiAtasRataRata = diAtasRataRata(penjualan, rataRata)
+
+print("\n===== Hasil Analisis =====")
+print("Total penjualan        :", total)
+print("Penjualan tertinggi    :", tertinggi[0], "(", tertinggi[1], ")")
+print("Rata-rata penjualan    :", round(rataRata, 2))
+print("Di atas rata-rata      :", jumlahDiAtasRataRata, "barang")
