b411467af773e6c1ef0f1ea0677713d45386553f
marchellotpp12 <marchellotpp12@gmail.com>
2026-09-10 15:53:24 +0700

    71251227

--- UG1-AnalisisAlgoritma-Rekursif/Soal#1/SortNim.py
+++ UG1-AnalisisAlgoritma-Rekursif/Soal#1/SortNim.py
@@ -1,31 +1,18 @@
 def InsertRecursive(sorted_array, current_value, current_length):
     # TODO 1: Implementasikan base case, logika komparasi pengurutan sesuai NIM, 
     # dan pemanggilan rekursi fungsi insert.
-    if current_length == 0 or sorted_array[current_length - 1] <= current_value:
-        return sorted_array[:current_length] + [current_value] + sorted_array[current_length:]
-    else:
-        return InsertRecursive(sorted_array, current_value, current_length - 1)
-        
+    pass
+
 
 def RecursiveFilterSort(data_array, current_length):
     # TODO 2: Implementasikan base case, pemecahan rekursif, dan filter kondisional 
     # untuk memanggil fungsi InsertRecursive sesuai paritas NIM.
-    if current_length == 0:
-        return []
-    else:
-        previous_result = RecursiveFilterSort(data_array, current_length - 1)
-        current_value = data_array[current_length - 1]
- 
-        # NIM berakhiran GANJIL -> ambil HANYA angka ganjil
-        if current_value % 2 != 0:
-            return InsertRecursive(previous_result, current_value, len(previous_result))
-        else:
-            return previous_result
+    pass
 
 
 # Ganti Dengan NIM Anda
 # Contoh, NIM_MAHASISWA = "71230994" -> nanti outputnya [4, 2, 0] 
-NIM_MAHASISWA = "71251227"
+NIM_MAHASISWA = ""
 
 if NIM_MAHASISWA != "":
     raw_data = [int(digit) for digit in NIM_MAHASISWA]
@@ -34,11 +21,4 @@ if NIM_MAHASISWA != "":
     final_result = RecursiveFilterSort(raw_data, data_length)
 
     # TODO 3: cetak hasil akhir sesuai format yang diminta.
-    last_digit = int(NIM_MAHASISWA[-1])
-    tipe = "GANJIL (Ascending)" if last_digit % 2 != 0 else "GENAP (Descending)"
- 
-    print("===== FILTER & SORT NIM =====")
-    print("NIM Mahasiswa   :", NIM_MAHASISWA)
-    print("Tipe            :", tipe)
-    print("Data Digit Awal :", raw_data)
-    print("Hasil Akhir     :", final_result)
+    pass
--- UG1-AnalisisAlgoritma-Rekursif/Soal#2/SistemKopdes.py
+++ UG1-AnalisisAlgoritma-Rekursif/Soal#2/SistemKopdes.py
@@ -2,32 +2,16 @@
 import random
 
 
-def totalPenjualan(data, n):
-    if n == 0:
-        return 0
-    else:
-        namabarang, jumlah = data[n - 1]
-        return jumlah + totalPenjualan(data, n - 1)
+def totalPenjualan(?, ?):
+    # kerjakan di sini
 
 
-def penjualanTertinggi(data, n):
-    if n == 1:
-        return data[0]
-    else:
-        sisaTertinggi = penjualanTertinggi(data, n - 1)
-        namaBarang, jumlah = data[n - 1]
-        if jumlah > sisaTertinggi[1]:
-            return (namaBarang, jumlah)
-        else:
-            return sisaTertinggi
+def penjualanTertinggi(?, ?):
+    # kerjakan di sini
 
 
-def diAtasRataRata(penjualan, rataRata):
-    jumlahDiAtas = 0
-    for namaBarang, jumlah in penjualan.items():
-        if jumlah > rataRata:
-            jumlahDiAtas += 1
-    return jumlahDiAtas
+def diAtasRataRata(?, ?):
+    # kerjakan di sini
 
 
 # Program Utama - Jangan dihapus/diedit yak
